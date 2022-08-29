c71a24db0c20c17fa9a1183e3c647e3d0b57966e
Lipo <lipo6636@gmail.com>
2022-08-30 00:16:56 +0200

    versionner 1

---
+++ projet.py
@@ -0,0 +1,9 @@
+print("je m'appelle moi-même")
+print("""je m'appelle
+Lipo kolie
+je suis étudiant
+je suis triste
+""")
+print("je m'appelle\nlipo")
+print("\u2764")
+print("c'est la version 2 du cours")
